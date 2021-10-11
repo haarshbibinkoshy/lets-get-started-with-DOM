836afadb15f9e1396bf6b1fbd6b7052bfd74308c
haarsh bibin koshy <haarshbibinkoshy.4127.92@gmail.com>
2021-10-11 12:28:34 +0530

    assignment

---
+++ dom.js
@@ -0,0 +1,23 @@
+// console.dir(document)
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title= 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[11]);
+// // document.all[11].textContent=`hello`
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.getElementById(`header-title`));
+let headerTitle = document.getElementById(`header-title`)
+// console.log(headerTitle);
+// headerTitle.style.borderBottom=`solid 3px #000`
+let header=document.getElementById(`main-header`)
+header.style.borderBottom=`solid 3px #000`
+let title=document.getElementById(`title`)
+title.style.fontWeight=`bold`
+title.style.color=`green`
